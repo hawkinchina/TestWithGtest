f727e04969ffad51a7dba4cd1227772413cbe7f1
yanbinghu <huyanbingcn@gmail.com>
2017-08-06 15:58:39 +0800

    change include

--- gtest/main/TestAll.cpp
+++ gtest/main/TestAll.cpp
@@ -1,12 +1,12 @@
 /****************************************************************   Copyright (C) 2017 All rights reserved.
-*   
+*
 *   文件名称：TestAll.cpp
 *   创 建 者：hyb
 *   创建日期：2017年07月26日
 *   描    述：
 *
 ***************************************************************/
-#include "gtest/gtest.h"
+#include "googletest.h"
 #include <iostream>
 using namespace std;
 int main(int argc,char* argv[])
